e93e6bf24dfd3c20e5d8f28951c85284edb33f1d
Automation <kalyan.parnandi@gmail.com>
2025-09-13 09:58:50 +0530

    Commit and Push 1

---
+++ Ex_02_JavaBasics/Lab002_Comments.java
@@ -0,0 +1,7 @@
+package Ex_02_JavaBasics;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        System.out.println("Hello 1"); // Single Line Comment
+    }
+}
---
+++ Ex_02_JavaBasics/Lab002_Comments_Multi.java
@@ -0,0 +1,14 @@
+package Ex_02_JavaBasics;
+
+public class Lab002_Comments_Multi {
+    public static void main(String[] args) {
+        /*
+        * This is Multi Comment
+        * Author : Kalyan
+        * */
+
+        //System.out.println("Hello 1");
+        //System.out.println("Hello 2");
+    }
+
+}
